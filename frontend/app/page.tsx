abeb2ed71b40373e29686870634b0679a46a4d17
nsitapara <nsitapara@gmail.com>
2025-01-19 22:18:03 -0800

    add a button to test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
@@ -47,6 +49,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button>Shadcn Test Button</Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
