354e62a54fef33890a6c12444fb485c8f3144afc
nsitapara <nsitapara@gmail.com>
2025-01-22 19:56:06 -0800

    add pre-commit hooks and run

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    authSource: 'admin',
-})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Error connecting to MongoDB:', err));
+    authSource: "admin",
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-
-app.get('/', (req, res) => {
-    res.send("API is running");
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
