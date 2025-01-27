021b8290deda17079ca61d4d3bb470cb87cf8619
nsitapara <nsitapara@gmail.com>
2025-01-27 12:28:20 -0800

    run pre-commit

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,33 +1,32 @@
-'use client'
+"use client";
 import { Button } from "@/components/ui/button";
 import { io } from "socket.io-client";
 import { useEffect } from "react";
 const socket = io("http://localhost:5001");
 
 function onClick() {
-    socket.emit("message", "Hello, server!");
-    console.log("Message sent");
+  socket.emit("message", "Hello, server!");
+  console.log("Message sent");
 }
 
 export default function Chat() {
-    useEffect(() => {
-        // Connection listener
-        socket.on("connect", () => {
-            console.log("Connected to server");
-        });
+  useEffect(() => {
+    // Connection listener
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
 
-        // Message listener
-        socket.on("message", (message) => {
-            console.log("Received message:", message);
-        });
+    // Message listener
+    socket.on("message", (message) => {
+      console.log("Received message:", message);
+    });
 
-        // Cleanup function to remove listeners when component unmounts
-        return () => {
-            socket.off("connect");
-            socket.off("message");
-        };
-    }, []);
+    // Cleanup function to remove listeners when component unmounts
+    return () => {
+      socket.off("connect");
+      socket.off("message");
+    };
+  }, []);
 
-    return <Button onClick={onClick}>Chat</Button>;
+  return <Button onClick={onClick}>Chat</Button>;
 }
-
