54e8742d9adcb863ca93ad3cc40c9352209633cf
nsitapara <nsitapara@gmail.com>
2025-02-03 22:35:01 -0800

    update model to support user list

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -10,10 +10,25 @@ const messageSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+  senderName: {
+    type: String,
+    required: true,
+  },
+  channelId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Channel",
+    required: true,
+    index: true,
+  },
   timestamp: {
     type: Date,
     default: Date.now,
+    index: true,
   },
 });
 
-export default mongoose.model("Message", messageSchema);
+messageSchema.index({ channelId: 1, timestamp: 1 });
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
